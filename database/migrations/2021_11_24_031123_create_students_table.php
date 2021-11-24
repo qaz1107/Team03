f056697460e683efa6e245e8b79b539ab6c3b8ca
DESKTOP-UITHU5S\F309 <d1094112014@gm.lhu.edu.tw>
2021-11-24 15:28:11 +0800

    建立表單

---
+++ database/migrations/2021_11_24_031123_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id()->comment('編號');
+            $table->integer('student_id')->unsigned()->comment('學號');
+            $table->tinyInteger('seat_number')->unsigned()->comment('座號');
+            $table->string('name',100)->comment('姓名');
+            $table->string('gender',10)->comment('性別');
+            $table->tinyInteger('cid')->unsigned()->comment('班級');
+            $table->date('graduation_year')->comment('畢業年份');
+            $table->date('start_year')->comment('入學年份');
+            $table->string('seat',20)->comment('座位');
+            $table->string('country',100)->comment('出生地');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ database/migrations/2021_11_24_031248_create_classes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('department',50)->comment('系別');
+            $table->string('classname',10)->comment('班級名稱');
+            $table->integer('grade')->unsigned()->comment('年級');
+            $table->string('classroom',50)->comment('教室');
+            $table->string('teacher',100)->comment('班導');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
