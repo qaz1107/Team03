cf5ba69b560b0d7e50c108b6660b2364c9ed50ec
DESKTOP-UITHU5S\F309 <d1094112014@gm.lhu.edu.tw>
2021-12-01 11:12:55 +0800

    View建立

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Hello World";
 });
+Route::resource('Students','StudentsController');
+Route::resource('classes','classesController');
