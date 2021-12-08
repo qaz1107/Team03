d0c9249cd24d65107f0447e50cd3cb6ed80b1452
DESKTOP-GD19ETQ\User <d1094112014@gm.lhu.edu.tw>
2021-12-08 11:28:18 +0800

    完成Seeder

--- database/seeders/ClassesTableSeeder.php
+++ database/seeders/ClassesTableSeeder.php
@@ -12,15 +12,36 @@ class  ClassesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function RandomDepartment()
     {
-        //
-        DB::table('classes')->insert([
-            'department'=> '文',
-            'classname'=> 'A班',
-            'grade'=> 1,
-            'classroom'=> 'C101',
-            'teacher'=> '羅慧君',
-        ]);
+        $positions = ['文', '理'];
+        return $positions[rand(0, count($positions) - 1)];
+    }
+    public function RandomClassname(){
+        $positions = ['A班', 'B班'];
+        return $positions[rand(0, count($positions) - 1)];
+    }
+    public function RandomClassroom(){
+        $positions = ['C101', 'C102'];
+        return $positions[rand(0, count($positions) - 1)];
+    }
+    public function RandomTeacher(){
+        $positions = ['羅慧君', '李思聰'];
+        return $positions[rand(0, count($positions) - 1)];
+    }
+    public function run(){
+        for ($i = 0; $i < 20; $i++) {
+            $department = $this->RandomDepartment();
+            $classname = $this->RandomClassname();
+            $classroom = $this->RandomClassroom();
+            $teacher = $this->RandomTeacher();
+            DB::table('classes')->insert([
+                'department' => $department,
+                'classname' => $classname,
+                'grade' => 1,
+                'classroom' => $classroom,
+                'teacher' => $teacher,
+            ]);
+        }
     }
 }
--- database/seeders/StudentsTableSeeder.php
+++ database/seeders/StudentsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,19 +13,52 @@ class StudentsTableSeeder extends Seeder
      *
      * @return void
      */
+    public function RandomStudentId(){
+        $first = rand(210001,210020);
+        $student_id = 'S' . $first;
+        return $student_id;
+    }
+    public function RandomName(){
+        $positions = ['林冠宇','赵秀秀','許鴻文','许慧语','戴鸿洁','梁幼萱','郑乐成','邵南莲','于明远','吕宜欣'
+                     ,'韩初瑶','彭正志','孟白梅','阎浩丽','张建弼','梁帅红','毛倩美','堵翔宇','宋静恬','梁浩宕'];
+        return $positions[rand(0,count($positions)-1)];
+    }
+    public function RandomGender(){
+        $positions = ['M','F'];
+        return $positions[rand(0,count($positions)-1)];
+    }
+    public function RandomSeat(){
+        $first = rand(0,2);
+        $last =rand(0,3);
+        $seat = $first . "," . $last;
+        return $seat;
+    }
+    public function RandomCountry(){
+        $positions = ['臺北','高雄','澎湖','臺南','臺中','花蓮'];
+        return $positions[rand(0,count($positions)-1)];
+    }
     public function run()
     {
-        //
-        DB::table('students')->insert([
-            'student_id' => 'S210001',
-            'seat_number' => 1,
-            'name' => '林冠宇',
-            'gender' => 'M',
-            'cid' => 1,
-            'graduation_year' => '2019-01-01',
-            'start_year' => '2020-01-01',
-            'seat' => '2,0',
-            'country' => '臺北',
-        ]);
+        for ($i=0;$i<20;$i++){
+            $student_id = $this->RandomStudentId();
+            $seat_number = rand(1,10);
+            $name = $this->RandomName();
+            $gender= $this->RandomGender();
+            $graduation_year = Carbon::now()->subYears(rand(11,15))->subMonths(rand(0,12))->subRealDays(rand(0,31));
+            $start_year = Carbon::now()->subYears(rand(4,10))->subMonths(rand(0,12))->subRealDays(rand(0,31));
+            $seat = $this->RandomSeat();
+            $country = $this->RandomCountry();
+            DB::table('students')->insert([
+                'student_id' => $student_id,
+                'seat_number' => $seat_number,
+                'name' => $name,
+                'gender' => $gender,
+                'cid' => rand(1,2),
+                'graduation_year' => $graduation_year,
+                'start_year' => $start_year,
+                'seat' => $seat,
+                'country' => $country,
+            ]);
+        }
     }
 }
